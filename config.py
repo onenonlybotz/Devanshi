a6c15d3201376a9e517a05e1207c94bde25538a0
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-11-23 11:41:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "YumikooBot")
+BOT_USERNAME = getenv("BOT_USERNAME", "DabangRobot")
 COMMAND_HANDLER = ["/", "!"]
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6691393517").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5999224089"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6144223366").split()))
 MONGO_URL = getenv("MONGO_URL", "")
 SESSION_STRING = getenv("SESSION_STRING", "")
